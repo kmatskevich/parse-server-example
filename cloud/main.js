1255246cd9deff33e0e15ee0afead953ec0412a2
Kirill Matskevich <inflamedbrain@gmail.com>
2016-07-08 11:53:11 +0300

    added new function getMessagesForUser (as example)

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,15 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('getMessagesForUser', function(request, response) {
+  var user = request.user; // request.user replaces Parse.User.current()
+  var token = user.getSessionToken(); // get session token from request.user
+
+  var query = new Parse.Query('Messages');
+  query.equalTo('recipient', user);
+  query.find({ sessionToken: token }) // pass the session token to find()
+    .then(function(messages) {
+      response.success(messages);
+    });
+});
