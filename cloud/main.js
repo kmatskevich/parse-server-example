6fd13688a786259ea114dababd8a61b8f76b999d
Kirill Matskevich <inflamedbrain@gmail.com>
2017-01-13 16:51:19 +0200

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,32 @@
 var facebookHelper = require('./facebookHelper');
 
 Parse.Cloud.define('collectDataFromFacebook', function(req, res) {
-	facebookHelper.loadDataFromFacebook(req, res);
+// 	facebookHelper.loadDataFromFacebook(req, res);
+console.log('loadDataFromFacebook start')
+	var user = request.user; 
+	if(user != nil){
+		console.log(user.name + 'come to the function');
+	}
+	var accessToken = user.get('authData')['facebook']['access_token'];
+	
+	if (Parse.FacebookUtils.isLinked(user)) {
+        Parse.Cloud.httpRequest({
+            url:'https://graph.facebook.com/me?fields=email,name,username&access_token='+user.get('authData').facebook.access_token,
+            success:function(httpResponse){
+                console.log(httpResponse.data.name);
+                console.log(httpResponse.data.email);
+                console.log(httpResponse.data.username);
+                res.success(httpResponse);
+            },
+            error:function(httpResponse){
+                console.error(httpResponse);
+                res.error(httpResponse);
+            }
+        });
+    }else{
+	    console.log(user.name + 'not linked');
+    }
+
 });
 
 Parse.Cloud.define('getMessagesForUser', function(request, response) {
