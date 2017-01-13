1e1cf5628fc6fa52781ce4c7c3cd78c63cca8577
Kirill Matskevich <inflamedbrain@gmail.com>
2017-01-13 14:21:58 +0200

    added facebookHelper

---
+++ cloud/facebookHelper.js
@@ -0,0 +1,18 @@
+Parse.cloude.define("loadDataFromFacebook", function(request, responce)){
+	var user = request.user; 
+	var accessToken = user.get('authData')['facebook']['access_token'];
+	
+	if (Parse.FacebookUtils.isLinked(user)) {
+        Parse.Cloud.httpRequest({
+            url:'https://graph.facebook.com/me?fields=email,name,username&access_token='+user.get('authData').facebook.access_token,
+            success:function(httpResponse){
+                console.log(httpResponse.data.name);
+                console.log(httpResponse.data.email);
+                console.log(httpResponse.data.username);
+            },
+            error:function(httpResponse){
+                console.error(httpResponse);
+            }
+        });
+    }
+}
--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,6 @@
 
+require('cloud/facebookHelper.js')
+
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
