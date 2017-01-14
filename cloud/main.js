3d2ed6b0355a8a8de3c87f40cbc73a87c36c1426
Kirill Matskevich <inflamedbrain@gmail.com>
2017-01-14 22:07:04 +0200

    no message

--- cloud/main.js
+++ cloud/main.js
@@ -3,15 +3,9 @@ var facebookHelper = require('./facebookHelper');
 Parse.Cloud.define('collectDataFromFacebook', function(req, res) {
 	var user = req.user; 
 	
- 	facebookHelper.loadDataFromFacebook(req).then(function(httpResponse) {
-			// success
-			console.log(httpResponse);
-			res.success(httpResponse);
-		},function(httpResponse) {
-			// error
-			console.error('Request failed with response code ' + httpResponse);
-			res.error(httpResponse);
-		});
+ 	facebookHelper.loadDataFromFacebook(req)
+ 	
+ 	res.success();
 
 /*
 	console.log('loadDataFromFacebook start')
